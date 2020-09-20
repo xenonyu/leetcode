1aaae6317c88494df31c0a9bdd0e727157ccc787
xmlmmfev <yumingxie46@gmail.com>
2020-09-20 18:56:29 +0800

    add answer doublePoint

--- DFS/pathSum.py
+++ DFS/pathSum.py
@@ -27,11 +27,26 @@ class Solution:
         recur(root, sum)
         return res
 
+    def pathSumII(self, root: TreeNode, sum: int):
+        def recur(root: TreeNode, preSum):
+            if not root: return
+            curSum = preSum + root.val
+            self.ans += hashMap.get(curSum - sum, 0)
+            hashMap[curSum] = hashMap.get(curSum, 0) + 1
+            recur(root.left, curSum)
+            recur(root.right, curSum)
+            hashMap[curSum] -= 1
+
+        hashMap = {0: 1}
+        self.ans = 0
+        recur(root, 0)
+        return self.ans
+
 
 if __name__ == "__main__":
-    inputList = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, 5, 1]
+    inputList = [1, -2, -3, 1, 3, -2, None, -1]
     test = Solution()
     root = parseListToTree(inputList)
     print_by_layer_1(root)
-    res = test.pathSum(root, 22)
+    res = test.pathSumII(root, 2)
     print(res)
